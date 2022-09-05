9b5bb32eb9f50363f16078eca5a71e8cffbf402d
viniciusMB <viniciusmartins@infojr.com.br>
2022-09-05 15:37:30 -0300

    chore: install dependencies

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,7 +1,5 @@
 import { EmailValidator } from '../presentation/protocols/email-validator'
 import validator from 'validator'
-
-// npm i validator && npm i @types/validator -D
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
     return validator.isEmail(email)
